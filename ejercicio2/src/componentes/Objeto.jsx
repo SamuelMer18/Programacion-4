b2526b85fccf1a246536b399ec47ed1aa744625e
Samuel Mercado <samuelmer18@hotmail.com>
2023-04-13 13:49:15 -0400

    ejercicio margenes y centrado de cards

--- ejercicio2/src/componentes/Objeto.jsx
+++ ejercicio2/src/componentes/Objeto.jsx
@@ -1,13 +1,14 @@
 import "./tarjeta.css";
 import imagen1 from "../imagenes/ironman.jpg";
 import imagen2 from "../imagenes/spiderman.jpg";
+import { Objeto2 } from "./Objeto2";
 
 const datos = [
-  { nombre: "Iron Man", fecha: "13/02/23", imagen: imagen2 },
+  { nombre: "Iron Man", fecha: "13/02/23", imagen: imagen1 },
   {
     nombre: "Spider Man",
     fecha: "13/02/23",
-    imagen: imagen1,
+    imagen: imagen2,
   },
 ];
 export function Objeto() {
@@ -17,6 +18,7 @@ export function Objeto() {
         <h3>{item.nombre}</h3>
         <h3>{item.fecha}</h3>
         <img className="imgtar" src={item.imagen} alt={item.nombre} />
+        <Objeto2 />
       </div>
     );
   });
---
+++ ejercicio2/src/componentes/Objeto2.jsx
@@ -0,0 +1,3 @@
+export function Objeto2() {
+  return <h1>Subcomponente</h1>;
+}
