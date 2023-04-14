61f5bd0d01de4b70d5cf3467cab6b378cb064bed
Samuel Mercado <samuelmer18@hotmail.com>
2023-04-14 12:59:15 -0400

    UseState & UseEffect Boton like dislike

---
+++ ejercicio2/src/componentes/Hoo.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import "./Hoo.css";
+export function Hoo() {
+  const [likes, setlikes] = useState(0);
+  const darlike = () => {
+    setlikes(likes + 1);
+  };
+  const dardislike = () => {
+    if (likes > 0) setlikes(likes - 1);
+  };
+  useEffect(dardislike);
+  return (
+    <div>
+      <h1>Los Likes de la Pagina Son: {likes}</h1>
+      <button class="button-22" onClick={darlike} variant="contained">
+        Dar Like
+      </button>
+      <button class="button-22" onClick={dardislike} variant="contained">
+        Dar DisLike
+      </button>
+    </div>
+  );
+}
--- ejercicio2/src/index.jsx
+++ ejercicio2/src/index.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import { Tarjeta } from "./componentes/Tarjeta";
 import { Objeto } from "./componentes/Objeto";
+import { Hoo } from "./componentes/Hoo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Hoo />
     <Tarjeta estado={false} />
     <Objeto />
   </React.StrictMode>
