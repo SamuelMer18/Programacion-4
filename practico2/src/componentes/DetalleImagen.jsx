1b46409ea906b50999fce99e293b1565c5fee024
Samuel Mercado <samuelmer18@hotmail.com>
2023-04-17 10:40:56 -0400

    detalleimagen estilos

--- practico2/src/componentes/DetalleImagen.jsx
+++ practico2/src/componentes/DetalleImagen.jsx
@@ -1,10 +1,16 @@
+import "./DetalleImagen.css";
+import { Link } from "react-router-dom";
+
 import { useLocation } from "react-router-dom";
 export function DetalleImagen() {
   const { state } = useLocation();
 
   return (
     <div className="card">
-      <img src={state.imagen} className="card-img-top" alt="..." />
+      <Link className="my-button1" to="/Producto">
+        Ir a Productos
+      </Link>
+      <img className="imagen" src={state.imagen} alt="..." />
       <div className="card-body">
         <h5 className="card-title">{state.descripcion}</h5>
         <p className="card-text">
