642d1126110ac96f33b47cccf1af3b808e42cd4d
Samuel Mercado <samuelmer18@hotmail.com>
2023-04-20 12:21:56 -0400

    Funciones Leer, Editar, Eliminar (solo funciones)

--- ejercicio5/src/App.jsx
+++ ejercicio5/src/App.jsx
@@ -1,16 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { collection, doc, query, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  query,
+  onSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
 import { db } from "./Data/Firebase";
 import { AddTodo } from "./components/AddTodo";
+import { Todo } from "./components/Todo";
 function App() {
   const [todos, setTodos] = useState([]);
-
+  //comandos para leer
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsubscribe();
+  }, []);
+  //comandos para Editar
+  const Edit = async (todo, title) => {
+    await updateDoc(doc(bd, "todos", todo.id), { title: title });
+  };
+  //comandos para Eliminar
+  const Eliminar = async (todo) => {
+    await deleteDoc(db, "todos", id);
+  };
   return (
-    <div>
-      <AddTodo />
+    <div className="App">
+      <div>
+        <AddTodo />
+      </div>
+      <div>
+        {todos.map((todo) => {
+          <Todo todo={todo} Editar={Edit} Eliminar={Eliminar} />;
+        })}
+      </div>
     </div>
   );
 }
